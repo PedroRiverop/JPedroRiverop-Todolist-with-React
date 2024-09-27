1fbb397d4b9b4a60e31554c578ef78372be28cf7
Pedro <jprivero4@gmail.com>
2024-09-27 15:45:41 -0400

    Pendiente completar estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+
+import React, { useState, useEffect } from 'react';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const [inputValue, setInputValue] = useState("");
+const [tasks, setTasks] = useState([]);
+
+const handleKeyPress = (event)=>{
+  if (event.key === 'Enter'){
+    setTasks([...tasks, inputValue]);
+    setInputValue("");
+  }
+}
+
+const handleClick = (index)=>{
+  setTasks(tasks.filter((elemento, i)=>i !== index))
+}
+
+	return(
+    <div className='d-flex justify-content-center'>
+      <div className='text-center my-5 border' style={{width: 500}}>
+        <h1 className='mb-3'>My To Do List...</h1>
+        <div className='mx-auto' style={{width: 200}}>
+          <input type="text" placeholder='Agregar Tarea...' value={inputValue} 
+            onChange={e=>setInputValue(e.target.value)} onKeyDown = {handleKeyPress} style={{width: 200}}/>
+          <ul className='myUl p-0'>{tasks.map((task, index)=>(<li className='d-flex justify-content-between' key={index}>{task}
+              <button className='myButton' onClick={()=>handleClick(index)}>X</button>
+                </li>))}</ul>
+        </div>
+        
+    </div>
+    </div>
+    
+  )
 };
 
 export default Home;
