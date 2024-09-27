96c86bfa52d3729a0ef5db5446d83ece2affdc8c
Pedro <jprivero4@gmail.com>
2024-09-27 19:03:05 -0400

    Proyecto terminado sin estilo de hojas sobrepuestas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -21,17 +21,18 @@ const handleClick = (index)=>{
 }
 
 	return(
-    <div className='d-flex justify-content-center'>
-      <div className='text-center my-5 border' style={{width: 500}}>
-        <h1 className='mb-3'>My To Do List...</h1>
-        <div className='mx-auto' style={{width: 200}}>
+    <div className='text-center'>
+       <h1 className='mb-1 mt-5 myTitle display-2'>My To Do List</h1>
+      <div className='text-start my-2 border mx-auto pb-2' style={{width: 500}}>
+       
+        <div className='mx-auto' style={{width: 400}}>
           <input type="text" placeholder='Agregar Tarea...' value={inputValue} 
-            onChange={e=>setInputValue(e.target.value)} onKeyDown = {handleKeyPress} style={{width: 200}}/>
-          <ul className='myUl p-0'>{tasks.map((task, index)=>(<li className='d-flex justify-content-between' key={index}>{task}
-              <button className='myButton' onClick={()=>handleClick(index)}>X</button>
+            onChange={e=>setInputValue(e.target.value)} onKeyDown = {handleKeyPress} style={{width: 400}} className='myInput fw-light mt-2'/>
+          <ul className='myUl p-0'>{tasks.map((task, index)=>(<li className="d-flex justify-content-between myListElement fw-light" key={index}>{task}
+              <button className='myButton' onClick={()=>handleClick(index)}>x</button>
                 </li>))}</ul>
         </div>
-        
+        <span className='fw-light mySpan m-2'>{tasks.length} Tareas pendientes</span>
     </div>
     </div>
     
