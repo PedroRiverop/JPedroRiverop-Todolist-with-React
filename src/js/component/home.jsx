8c178875662fe51e62c63a247f7a9ee4ee3cdd26
Rivero Peña Jose Pedro <129573462+PedroRiverop@users.noreply.github.com>
2024-09-30 03:09:08 +0000

    Correción de observaciones,backgrounds, font-sizes, etc.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState} from 'react';
 
 //create your first component
 const Home = () => {
@@ -24,19 +21,19 @@ const handleClick = (index)=>{
 	return(
     <div className='text-center myBodyCard'>
        <h1 className='mb-1 mt-5 myTitle display-2'>My To Do List</h1>
-      <div className='text-start mt-2 mb-0 border mx-auto pb-2 position-relative' style={{width: 500,}}>
+      <div className='text-start mt-2 mb-0 border mx-auto pb-2 position-relative bg-white' style={{width: 500,}}>
        
-      <div className='mx-auto bg-light' style={{width: 400}}>
+      <div className='myDivCard mx-auto bg-white'>
             <input type="text" placeholder='Agregar Tarea...' value={inputValue} 
-              onChange={e=>setInputValue(e.target.value)} onKeyDown = {handleKeyPress} style={{width: 400}} className='myInput fw-light mt-2'/>
+              onChange={e=>setInputValue(e.target.value)} onKeyDown = {handleKeyPress} className='myInput fw-light mt-2'/>
             <ul className='myUl p-0'>{tasks.map((task, index)=>(<li className="d-flex justify-content-between myListElement fw-light" key={index}>{task}
                 <button className='myButton' onClick={()=>handleClick(index)}>x</button>
                   </li>))}</ul>
           </div>
           <span className='fw-light mySpan m-2'>{tasks.length} Tareas pendientes</span>
       </div>
-      <div className='superpuesto border mx-auto' style={{width: 485, height: 8}}><br/></div>
-      <div className='superpuesto border mx-auto shadow' style={{width: 470, height: 8}}><br/></div>
+      <div className='variousEffect border mx-auto' style={{width: 485}}><br/></div>
+      <div className='variousEffect border mx-auto shadow' style={{width: 470}}><br/></div>
       
     </div>
     
